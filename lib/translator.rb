cbed7a04e05e446ed2109b6b538be9311a6d244d
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 22:07:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"
 
 def load_library(file_path)
   # code goes here
-  emoticons = YAML.load_file("emoticons.yml")
+  emoticons = YAML.load_file(file_path)
 end
 
 def get_japanese_emoticon
