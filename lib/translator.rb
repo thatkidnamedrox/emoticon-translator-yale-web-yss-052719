b186a1b4e9c84a41f89b24b668688dcd6c76a1f4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 22:18:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,11 @@ def load_library(file_path)
   # code goes here
   emoticons = YAML.load_file(file_path)
   emoticons = emoticons.map do |meaning, emoticons|
-    { "get_meaning"=>meaning, "get_emoticon"=>emoticons }
+    { 
+      "get_meaning"=>meaning, 
+      "get_emoticon"=>
+        emoticons.first => emoticons.last
+    }
   end
   puts emoticons
   emoticons
