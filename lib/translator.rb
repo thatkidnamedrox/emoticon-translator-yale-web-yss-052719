35a561f77a8bb69bace73f6264ec55107ca5b1c0
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 22:29:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(file_path)
   emoticons["get_meaning"] = {}
   emoticons["get_emoticon"] = {}
   library.each do |meaning, emoticons|
+    puts emoticons
     emoticons["get_meaning"][emoticons.last] = meaning
     emoticons["get_emotion"][emoticons.first] = emoticons.last
   end
